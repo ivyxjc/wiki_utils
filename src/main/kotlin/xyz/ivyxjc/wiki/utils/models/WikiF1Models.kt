2d73c45cae0d69d27df374d8b0b67598f516e134
ivyxjc <ivyxjc1994@hotmail.com>
2019-08-06 23:39:18 +0800

    :tada:

---
+++ src/main/kotlin/xyz/ivyxjc/wiki/utils/models/WikiF1Models.kt
@@ -0,0 +1,38 @@
+package xyz.ivyxjc.wiki.utils.models
+
+import org.apache.commons.lang3.builder.ToStringBuilder
+
+
+class F1Driver(val number: Int) {
+    lateinit var name: String
+    var rank: Int = Int.MIN_VALUE
+    var pole = false
+    var fastestLap = false
+
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this)
+    }
+}
+
+class F1Team(val name: String) {
+    lateinit var driver1: F1Driver
+    lateinit var driver2: F1Driver
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this)
+    }
+}
+
+class F1Match(val name: String) {
+    var f1Teams = mutableMapOf<String, F1Team>()
+    var f1Drivers = mutableMapOf<Int, F1Driver>()
+    var pole = F1Driver(-1)
+    var fastedLap = F1Driver(-1)
+
+    override fun toString(): String {
+        return ToStringBuilder.reflectionToString(this)
+    }
+}
+
+class F1Season(val year: String) {
+    lateinit var matches: MutableMap<String, F1Match>
+}
